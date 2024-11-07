28381946857a3335c5ff1b31c945eb081e11a375
Tan-dat2001 <nguyentandat23122001@gmail.com>
2024-11-07 23:06:54 +0700

    init project

---
+++ src/test/java/com/datnt194/project_management_system/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datnt194.project_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
